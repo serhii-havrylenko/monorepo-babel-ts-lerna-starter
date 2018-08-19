fc1207395cab4b4b5ba5f08d8beee85953123ac1
chef <serhii.havrylenko@gmail.com>
2018-08-19 20:03:07 +0200

    update all dependencies and fix @babel config

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = api => {
+  api.cache(true);
+
+  return {
+    presets: [
+      [
+        "@babel/env",
+        {
+          targets: {
+            browsers: "Last 2 Chrome versions, Firefox ESR",
+            node: "8.9"
+          }
+        }
+      ],
+      "@babel/preset-react",
+      "@babel/preset-typescript"
+    ],
+    ignore: ["node_modules"]
+  };
+};
--- packages/login-form/src/LoginForm.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-const wrapText = (text) => React.createElement("span", null,
-    "$",
-    text);
-export const LoginForm = () => React.createElement("div", null,
-    "$",
-    wrapText("Login form"));
