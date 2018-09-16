a96cadf4ee9932b620a12dcd4d228642f7d7cf6a
chef <serhii.havrylenko@gmail.com>
2018-09-16 12:06:49 +0200

    add linters

--- packages/login-form/src/LoginForm.tsx
+++ packages/login-form/src/LoginForm.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
+import styled from "styled-components";
 
-const wrapText = (text: string) => <span>${text}</span>;
+const BoldText = styled.span`
+  font-weight: bold;
+`;
+const wrapText = (text: string) => <BoldText>${text}</BoldText>;
 
 export const LoginForm: React.SFC = () => <div>${wrapText("Login form")}</div>;
--- packages/login-form/src/index.ts
+++ packages/login-form/src/index.ts
@@ -1,1 +1,1 @@
-export * from "./LoginForm";
+export * from './LoginForm';
